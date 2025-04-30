e58581de04e7620d7b80bf3b1dbdc093b8cd9430
Seo-Yegyeong <seoye6618@gmail.com>
2025-04-30 11:28:35 +0900

    [TEST] Create a button with the event handler

--- CsharpStudying/Form1.Designer.cs
+++ CsharpStudying/Form1.Designer.cs
@@ -28,41 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox_input
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 21);
-            this.textBox1.TabIndex = 0;
+            this.textBox_input.Location = new System.Drawing.Point(34, 12);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(220, 51);
+            this.textBox_input.TabIndex = 0;
             // 
-            // textBox2
+            // textBox_result
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 135);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 1;
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(34, 141);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(454, 140);
+            this.textBox_result.TabIndex = 2;
             // 
-            // button1
+            // button_input
             // 
-            this.button1.Location = new System.Drawing.Point(275, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_input.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_input.Location = new System.Drawing.Point(272, 12);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 51);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(52, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "true";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(123, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "false";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 54);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(527, 367);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox_input);
             this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -71,9 +118,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- CsharpStudying/Form1.cs
+++ CsharpStudying/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,13 @@ namespace CsharpStudying
 {
     public partial class Form1 : Form
     {
+        private string boolValue;
+
         public Form1()
         {
-            InitializeComponent();
 
+            InitializeComponent();
+            
             //string[,] order = new string[3, 4]
             //{
             //    {"홍길동", "포도", "복숭아", "바나나"},
@@ -48,8 +52,50 @@ namespace CsharpStudying
 
         }
 
-        
+        bool Coin(bool coinTF)
+        {
+            Random random = new Random();
+            int result = random.Next(0, 100) % 2;
+            if ((result == 1 && coinTF == true) || (result == 0 && coinTF == false))
+            {
+                return true;
+            }
+            return false;
+        }
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            #region #Debugging Test
+            Console.WriteLine("textBox : " + textBox_input.Text + "radioButton : " + boolValue);
+            Console.WriteLine("textBox : " + (textBox_input.Text == "") + " radioButton : " + (boolValue==null));
+            #endregion
 
-        
+            if (textBox_input.Text == "" || this.boolValue == null)
+            {
+                textBox_result.Text = "You didn't pick any option!";
+                return;
+            }
+            else if (textBox_input.Text != "true" && textBox_input.Text != "false")
+            {
+                textBox_result.Text = "invalid value!";
+                return;
+            }
+
+            if (textBox_input.Text != "")
+            {
+                boolValue = textBox_input.Text;
+            }
+
+            textBox_result.Text = Coin(Convert.ToBoolean(boolValue)).ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            boolValue = "true";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            boolValue = "false";
+        }
     }
 }
