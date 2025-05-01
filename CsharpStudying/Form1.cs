d87ad21fec748b08f47d7df8d9c77fe84f63e3e2
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-01 10:56:44 +0900

    [TEST] multiple for statement

--- CsharpStudying/Form1.cs
+++ CsharpStudying/Form1.cs
@@ -15,23 +15,16 @@ namespace CsharpStudying
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
-            int cnt = 0;
-            int sum = 0;
-
-            for(int i = 1; i <= 100; i++)
+            for (int i = 2; i <= 9; i++)
             {
-                if(i % 3 == 0 && i % 5 != 0)
+                for (int j = 1;  j <= 9; j++)
                 {
-                    cnt++;
-                    sum += i;
-                    Console.WriteLine(i + "\r\n");
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
                 }
             }
-            Console.WriteLine("cnt: " + cnt + " sum: " + sum);
         }
     }
 }
