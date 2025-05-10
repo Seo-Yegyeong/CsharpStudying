973b38a7fe034410dbad524160a5904235715da3
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-10 23:34:15 +0900

    chore: mv the code to Storage.cs

--- CsharpStudying/Storage.Designer.cs
+++ CsharpStudying/Storage.Designer.cs
@@ -37,6 +37,14 @@
             this.button_input = new System.Windows.Forms.Button();
             this.textBox_result = new System.Windows.Forms.TextBox();
             this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_comScore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_myScore = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonPaper = new System.Windows.Forms.Button();
+            this.buttonRock = new System.Windows.Forms.Button();
+            this.buttonScissors = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox3
@@ -46,11 +54,11 @@
             this.textBox3.Font = new System.Drawing.Font("NanumGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.textBox3.ForeColor = System.Drawing.SystemColors.InfoText;
             this.textBox3.HideSelection = false;
-            this.textBox3.Location = new System.Drawing.Point(36, 319);
+            this.textBox3.Location = new System.Drawing.Point(36, 207);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(197, 103);
+            this.textBox3.Size = new System.Drawing.Size(139, 66);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = "0";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -62,11 +70,11 @@
             this.textBox2.Font = new System.Drawing.Font("NanumGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.InfoText;
             this.textBox2.HideSelection = false;
-            this.textBox2.Location = new System.Drawing.Point(36, 188);
+            this.textBox2.Location = new System.Drawing.Point(36, 131);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(197, 103);
+            this.textBox2.Size = new System.Drawing.Size(139, 66);
             this.textBox2.TabIndex = 6;
             this.textBox2.Text = "0";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -92,7 +100,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(197, 103);
+            this.textBox1.Size = new System.Drawing.Size(139, 66);
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = "0";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -100,7 +108,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(263, 131);
+            this.radioButton1.Location = new System.Drawing.Point(203, 121);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(44, 16);
             this.radioButton1.TabIndex = 11;
@@ -111,7 +119,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(339, 131);
+            this.radioButton2.Location = new System.Drawing.Point(279, 121);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(50, 16);
             this.radioButton2.TabIndex = 12;
@@ -122,9 +130,9 @@
             // button_input
             // 
             this.button_input.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button_input.Location = new System.Drawing.Point(407, 59);
+            this.button_input.Location = new System.Drawing.Point(321, 57);
             this.button_input.Name = "button_input";
-            this.button_input.Size = new System.Drawing.Size(75, 51);
+            this.button_input.Size = new System.Drawing.Size(61, 33);
             this.button_input.TabIndex = 9;
             this.button_input.Text = "Input";
             this.button_input.UseVisualStyleBackColor = true;
@@ -132,26 +140,114 @@
             // textBox_result
             // 
             this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.textBox_result.Location = new System.Drawing.Point(259, 163);
+            this.textBox_result.Location = new System.Drawing.Point(203, 153);
             this.textBox_result.Multiline = true;
             this.textBox_result.Name = "textBox_result";
             this.textBox_result.ReadOnly = true;
-            this.textBox_result.Size = new System.Drawing.Size(223, 138);
+            this.textBox_result.Size = new System.Drawing.Size(179, 120);
             this.textBox_result.TabIndex = 10;
             // 
             // textBox_input
             // 
-            this.textBox_input.Location = new System.Drawing.Point(259, 59);
+            this.textBox_input.Location = new System.Drawing.Point(199, 55);
             this.textBox_input.Multiline = true;
             this.textBox_input.Name = "textBox_input";
-            this.textBox_input.Size = new System.Drawing.Size(130, 51);
+            this.textBox_input.Size = new System.Drawing.Size(116, 33);
             this.textBox_input.TabIndex = 8;
+            // 
+            // textBox_comScore
+            // 
+            this.textBox_comScore.Location = new System.Drawing.Point(589, 184);
+            this.textBox_comScore.Multiline = true;
+            this.textBox_comScore.Name = "textBox_comScore";
+            this.textBox_comScore.Size = new System.Drawing.Size(50, 23);
+            this.textBox_comScore.TabIndex = 20;
+            this.textBox_comScore.Text = "0";
+            this.textBox_comScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(595, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "마왕";
+            // 
+            // textBox_myScore
+            // 
+            this.textBox_myScore.Location = new System.Drawing.Point(421, 184);
+            this.textBox_myScore.Multiline = true;
+            this.textBox_myScore.Name = "textBox_myScore";
+            this.textBox_myScore.Size = new System.Drawing.Size(50, 23);
+            this.textBox_myScore.TabIndex = 18;
+            this.textBox_myScore.Text = "0";
+            this.textBox_myScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(428, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "용사";
+            // 
+            // buttonPaper
+            // 
+            this.buttonPaper.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonPaper.Location = new System.Drawing.Point(573, 227);
+            this.buttonPaper.Name = "buttonPaper";
+            this.buttonPaper.Size = new System.Drawing.Size(66, 46);
+            this.buttonPaper.TabIndex = 16;
+            this.buttonPaper.Text = "보";
+            this.buttonPaper.UseVisualStyleBackColor = true;
+            // 
+            // buttonRock
+            // 
+            this.buttonRock.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonRock.Location = new System.Drawing.Point(498, 227);
+            this.buttonRock.Name = "buttonRock";
+            this.buttonRock.Size = new System.Drawing.Size(66, 46);
+            this.buttonRock.TabIndex = 15;
+            this.buttonRock.Text = "바위";
+            this.buttonRock.UseVisualStyleBackColor = true;
+            // 
+            // buttonScissors
+            // 
+            this.buttonScissors.Font = new System.Drawing.Font("GulimChe", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonScissors.Location = new System.Drawing.Point(421, 227);
+            this.buttonScissors.Name = "buttonScissors";
+            this.buttonScissors.Size = new System.Drawing.Size(66, 46);
+            this.buttonScissors.TabIndex = 14;
+            this.buttonScissors.Text = "가위";
+            this.buttonScissors.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox4.Location = new System.Drawing.Point(404, 37);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(235, 114);
+            this.textBox4.TabIndex = 13;
             // 
             // Storage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(678, 450);
+            this.Controls.Add(this.textBox_comScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_myScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonPaper);
+            this.Controls.Add(this.buttonRock);
+            this.Controls.Add(this.buttonScissors);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.button_input);
@@ -179,5 +275,13 @@
         private System.Windows.Forms.Button button_input;
         private System.Windows.Forms.TextBox textBox_result;
         private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_comScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_myScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonPaper;
+        private System.Windows.Forms.Button buttonRock;
+        private System.Windows.Forms.Button buttonScissors;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- CsharpStudying/Storage.cs
+++ CsharpStudying/Storage.cs
@@ -547,5 +547,109 @@ namespace CsharpStudying // 프로젝트 이름.
             return name + "의 점수: " + score + "점\r\n";
         }
         #endregion
+
+        #region #Rock Paper Scissor shoot game
+        // 가위 / 바위 / 보
+        enum RPS
+        {
+            Rock,
+            Paper,
+            Scissors,
+            fail = 100
+        }
+
+        // 승부 결과 (승리 / 패배 / 비김)
+        enum Result
+        {
+            Win,
+            Lose,
+            Same
+        }
+
+        int userScore = 0;
+        int comScore = 0;
+
+
+        // (가위, 바위, 보) 버튼 클릭 시 실행 로직
+        private void RPS_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = ""; //get rid of pre-records.
+            RPS mine = CheckRPS(((Button)sender).Name); //What button did the user click?
+            PlayRPSGrame(mine); //Get the result of 'Rock-Paper-Scissors'
+        }
+
+        // 게임 전체 로직
+        void PlayRPSGrame(RPS mine)
+        {
+            Result result = RPSGame(mine);
+            if (result == Result.Win)
+            {
+                userScore++;
+                textBox_myScore.Text = userScore.ToString();
+            }
+            else if (result == Result.Lose)
+            {
+                comScore++;
+                textBox_comScore.Text = comScore.ToString();
+            }
+
+            if (userScore == 3 || comScore == 3)
+            {
+                textBox_result.Text += "게임 끝!";
+                SetToZero();
+            }
+        }
+
+        // 승부 판단 로직
+        Result RPSGame(RPS mine)
+        {
+            Random r = new Random();
+            RPS computer = (RPS)r.Next(0, 3);
+
+            textBox_result.Text += $"마왕의 선택: {computer}\r\n";
+            if (mine == computer)
+            {
+                textBox_result.Text += "비겼습니다!\r\n";
+                return Result.Same;
+            }
+            else if ((mine == RPS.Rock && computer == RPS.Scissors) || (mine == RPS.Paper && computer == RPS.Rock) || (mine == RPS.Scissors && computer == RPS.Paper))
+            {
+
+                textBox_result.Text += "이겼어요 야호~~!\r\n";
+                return Result.Win;
+            }
+            else
+            {
+                textBox_result.Text += "마왕의 득점! 내가 봐줬다~~\r\n";
+                return Result.Lose;
+            }
+        }
+
+        // 점수 초기화 로직
+        void SetToZero()
+        {
+            textBox_myScore.Text = "0";
+            textBox_comScore.Text = "0";
+        }
+
+
+        // User가 선택한 옵션값을 ENUM 값으로 반환
+        RPS CheckRPS(string mine)
+        {
+            switch (mine)
+            {
+                case "buttonScissors":
+                    return RPS.Scissors;
+                case "buttonRock":
+                    return RPS.Rock;
+                case "buttonPaper":
+                    return RPS.Paper;
+                default:
+                    return RPS.fail;
+            }
+        }
+        #endregion
+
+
     }
 }
