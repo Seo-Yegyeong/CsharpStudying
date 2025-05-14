9053f376b60054966bab6807c65bbb1a7e76c38d
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-14 12:36:56 +0900

    feat: Implement the validation on the nickname function

--- CsharpStudying/Form3.cs
+++ CsharpStudying/Form3.cs
@@ -16,6 +16,33 @@ namespace CsharpStudying
         {
             InitializeComponent();
 
+            try
+            {
+                Console.Write("Input your new Nickname : ");
+                string nickname = Console.ReadLine();
+
+                if (nickname.Length == 0)
+                {
+                    throw new Exception("닉네임을 입력해주세요!");
+                }
+                if (nickname.Length < 2)
+                {
+                    throw new Exception("닉네임은 2글자 이상이어야 합니다!");
+                }
+                if (nickname.Contains("admin"))
+                {
+                    throw new Exception("닉네임에 'admin'은 포함될 수 없습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램을 종료합니다.");
+            }
+
         }
     }
 }
