b382984a42fb3e79d1c2aff1aa919e8f3d98431d
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-20 08:36:37 +0900

    feat: Practice BackgroundWorker Usage

---
+++ CsharpStudying/Form7MultiThread.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CsharpStudying
+{
+    partial class Form7MultiThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnStart.Location = new System.Drawing.Point(43, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(254, 165);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(41, 244);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 12);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "lblStatus";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(46, 276);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(341, 45);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(425, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 300);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form7MultiThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form7MultiThread";
+            this.Text = "Form7MultiThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CsharpStudying/Form7MultiThread.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; // For Thread.Sleep
+
+namespace CsharpStudying
+{
+    public partial class Form7MultiThread : Form
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+        //static int sharedData = 0; // Static variable to keep track of the count
+
+        public Form7MultiThread()
+        {
+            InitializeComponent();
+            worker.WorkerReportsProgress = true; // Enable progress reporting
+            worker.WorkerSupportsCancellation = false; // Disable cancellation
+            worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+
+
+            #region # Thread Example
+            /*
+            Thread thread1 = new Thread(ThreadMethod1);
+            Thread thread2 = new Thread(new ThreadStart(ThreadMethod2));
+
+            thread1.Start();
+            thread2.Start();
+            */
+            #endregion
+
+        }
+
+
+        #region # Thread Methods
+        /*
+        private void ThreadMethod1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                sharedData++;
+                Thread.Sleep(10); // CPU 점유 방지용 딜레이
+                textBox1.Text += $"1: {sharedData}\r\n";
+            }
+        }
+
+        private void ThreadMethod2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                sharedData++;
+                Thread.Sleep(10); // Simulate work
+                textBox1.Text += $"2: {sharedData}\r\n";
+            }
+        }*/
+        #endregion
+
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Perform a time-consuming operation here
+            for (int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(30); // Simulate work
+                worker.ReportProgress(i); // Report progress
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!worker.IsBusy)
+            {
+                lblStatus.Text = "Processing...";
+                progressBar1.Value = 0; // Reset progress bar
+                worker.RunWorkerAsync(); // Start the background worker
+            }
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage; // Update progress bar //e.ProgressPercentage는 ReportProgress에서 전달한 값. %로 전달.
+            lblStatus.Text = $"Progress: {e.ProgressPercentage}%"; // Update status label
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Completed!");
+            progressBar1.Value = 100; // Set progress bar to 100%
+        }
+    }
+}
