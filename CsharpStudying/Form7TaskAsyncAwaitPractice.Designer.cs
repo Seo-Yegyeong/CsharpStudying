b05e0dad27a2e66ba89e33c0e625c82add9e578b
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-20 12:23:07 +0900

    feat: touch a file using streamReader and ReadToEndAsync()

---
+++ CsharpStudying/Form7TaskAsyncAwaitPractice.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CsharpStudying
+{
+    partial class Form7TaskAsyncAwaitPractice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(40, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(482, 288);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // Form7TaskAsyncAwaitPractice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form7TaskAsyncAwaitPractice";
+            this.Text = "Form7TaskAsyncAwaitPractice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
---
+++ CsharpStudying/Form7TaskAsyncAwaitPractice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpStudying
+{
+    public partial class Form7TaskAsyncAwaitPractice : Form
+    {
+        public Form7TaskAsyncAwaitPractice()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+
+            openFileDialog.InitialDirectory = "C:\\";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                var fileStream = openFileDialog.OpenFile();
+
+                ReadFileAsync(filePath);
+            }            
+        }
+        private async Task ReadFileAsync(string filePath)
+        {
+            StreamReader sr = File.OpenText(filePath);
+            string fileContent = string.Empty;
+            
+            textBox1.Text = await sr.ReadToEndAsync();
+            
+            Console.WriteLine("The end of program!");
+            sr.Close();
+        }
+    }
+}
